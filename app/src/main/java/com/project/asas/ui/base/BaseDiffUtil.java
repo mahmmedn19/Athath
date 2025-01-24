782bab6b286fe0c3d66e2d1fd352f8ce5a1f17f2
Mohamed Naser <mahmmedn19@gmail.com>
2025-01-24 14:47:35 +0200

    add dependency

---
+++ app/src/main/java/com/project/asas/ui/base/BaseDiffUtil.java
@@ -0,0 +1,52 @@
+package com.project.asas.ui.base;
+
+
+import androidx.recyclerview.widget.DiffUtil;
+
+import java.util.List;
+import java.util.function.BiFunction;
+
+public class BaseDiffUtil<T> extends DiffUtil.Callback {
+    private final List<T> oldList;
+    private final List<T> newList;
+    private final ItemCheck<T> checkIfSameItem;
+    private final ItemCheck<T> checkIfSameContent;
+
+    public interface ItemCheck<T> {
+        boolean areItemsSame(T oldItem, T newItem);
+        boolean areContentsSame(T oldItem, T newItem);
+    }
+
+    public BaseDiffUtil(
+            List<T> oldList,
+            List<T> newList,
+            ItemCheck<T> checkIfSameItem,
+            ItemCheck<T> checkIfSameContent
+    ) {
+        this.oldList = oldList;
+        this.newList = newList;
+        this.checkIfSameItem = checkIfSameItem;
+        this.checkIfSameContent = checkIfSameContent;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return oldList.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return newList.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        return checkIfSameItem.areItemsSame(oldList.get(oldItemPosition), newList.get(newItemPosition));
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        return checkIfSameContent.areContentsSame(oldList.get(oldItemPosition), newList.get(newItemPosition));
+    }
+}
+
---
+++ app/src/main/java/com/project/asas/ui/base/BaseInteractionListener.java
@@ -0,0 +1,4 @@
+package com.project.asas.ui.base;
+
+public class BaseInteractionListener {
+}
