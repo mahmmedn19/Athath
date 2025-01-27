9a8fb41861ccec1c16f40be6b4feb889bdb75774
Mohamed Naser <m.nasser@trips.sa.com>
2025-01-27 22:40:25 +0200

    refactor

--- app/src/main/java/com/project/asas/ui/user_selection_fragment/UserSelectionFragment.java
+++ app/src/main/java/com/project/asas/ui/user_selection_fragment/UserSelectionFragment.java
@@ -2,13 +2,8 @@ package com.project.asas.ui.user_selection_fragment;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModel;
 import androidx.navigation.Navigation;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.project.asas.R;
 import com.project.asas.databinding.FragmentUserSelectionBinding;
